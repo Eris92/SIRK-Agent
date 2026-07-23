6363ed6dea0c9981c6c7c13b6d601194614dc2d8
Eris92 <43513086+Eris92@users.noreply.github.com>
2026-07-23 21:23:53 +0200

    feat(agent): add service host

---
+++ SIRK-Agent/src/SIRK.Agent/Program.cs
@@ -0,0 +1,18 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Sirk.Agent.Ipc;
+using Sirk.Agent.Protocol;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddWindowsService(options =>
+{
+    options.ServiceName = "SIRK Agent";
+});
+
+builder.Services.AddSingleton<ProtocolValidator>();
+builder.Services.AddSingleton<CommandDispatcher>();
+builder.Services.AddSingleton<NamedPipeCommandServer>();
+builder.Services.AddHostedService<AgentWorker>();
+
+await builder.Build().RunAsync();

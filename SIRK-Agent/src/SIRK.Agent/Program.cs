de7e64499847897d9863fefec6b09e123a626ded
Eris92 <43513086+Eris92@users.noreply.github.com>
2026-07-23 22:18:58 +0200

    agent: register modular System and Workspace handlers

--- SIRK-Agent/src/SIRK.Agent/Program.cs
+++ SIRK-Agent/src/SIRK.Agent/Program.cs
@@ -1,7 +1,9 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Sirk.Agent;
+using Sirk.Agent.Commands;
 using Sirk.Agent.Ipc;
+using Sirk.Agent.Modules.Workspace;
 using Sirk.Agent.Protocol;
 
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
@@ -13,6 +15,8 @@ builder.Services.AddWindowsService(options =>
 
 builder.Services.AddSingleton<ReplayProtection>();
 builder.Services.AddSingleton<ProtocolValidator>();
+builder.Services.AddSingleton<ICommandHandler, SystemCommandHandler>();
+builder.Services.AddSingleton<ICommandHandler, WorkspaceCommandHandler>();
 builder.Services.AddSingleton<CommandDispatcher>();
 builder.Services.AddSingleton<NamedPipeCommandServer>();
 builder.Services.AddHostedService<AgentWorker>();

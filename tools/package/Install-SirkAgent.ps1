2d4ae4a37b324da0bffbb648cf4edc817e67ce02
Krzysztof Lechmyc <krzysztof.lechmyc@gmail.com>
2026-07-28 09:47:43 +0200

    security: restrict Agent data directory writes

--- tools/package/Install-SirkAgent.ps1
+++ tools/package/Install-SirkAgent.ps1
@@ -36,6 +36,16 @@ Get-ChildItem -LiteralPath $source -File | Where-Object {
     $_.Name -notlike '*.zip' -and $_.Name -notlike 'TestBundle-*'
 } | Copy-Item -Destination $InstallPath -Force
 
+$dataPath = Join-Path $env:ProgramData 'SIRK\Agent'
+New-Item -ItemType Directory -Path $dataPath -Force | Out-Null
+& icacls.exe $dataPath /inheritance:r `
+    /grant:r '*S-1-5-18:(OI)(CI)F' `
+    '*S-1-5-32-544:(OI)(CI)F' `
+    '*S-1-5-32-545:(OI)(CI)RX' | Out-Null
+if ($LASTEXITCODE -ne 0) {
+    throw "Nie udalo sie zabezpieczyc ACL katalogu danych: $dataPath"
+}
+
 $targetExe = Join-Path $InstallPath $exeName
 & sc.exe create $ServiceName binPath= ('"{0}"' -f $targetExe) start= auto DisplayName= 'SIRK Agent' | Out-Null
 & sc.exe description $ServiceName 'SIRK Agent security runtime and diagnostics service.' | Out-Null
